0797eccba0652cdf9215d8cc90aaaf367c8a3976
emirssubasi <112613839+emirssubasi@users.noreply.github.com>
2023-02-22 23:31:56 +0300

    Localden

---
+++ kredikarti/README.md
@@ -0,0 +1,10 @@
+# $  npm install kart-dogrulama
+
+const card = require('kart-dogrulama');
+
+console.log(card('5374661040114189'));
+// true dondurur
+
+
+
+
---
+++ kredikarti/index.js
@@ -0,0 +1,21 @@
+function cardValidation(numbers) {
+    const input = validateUserInput(numbers);
+    if (input === false) {
+      return false;
+    } else {
+      return numbers.split('')
+        .reverse()
+        .map((num) => parseInt(num))
+        .map((num, index) => index % 2 ? num * 2 : num)
+        .map((num) => num > 9 ? (num % 10) + 1 : num)
+        .reduce((accum, num) => accum += num) % 10 === 0;
+    }
+  }
+  
+  function validateUserInput(userInput) {
+    if (userInput === '' || typeof userInput !== 'string') {
+      return false;
+    }
+  }
+  
+  module.exports = cardValidation;
